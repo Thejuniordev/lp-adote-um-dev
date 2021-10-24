8814a8c9c8f7f18f233e8a591846bf4a3fa77e0f
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-10-24 13:13:08 -0300

    fix: Add google fonts roboto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import Head from 'next/head'
 import { Header } from '../components/Header'
 
@@ -8,6 +9,9 @@ export default function Home() {
         <title>Adote um dev</title>
         <meta name="description" content="Adote um dev" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
       </Head>
       <Header />
     </>
